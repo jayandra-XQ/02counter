cb850bc81b6975acb9428c9b1c5e9a99fca677a3
Jayandra Hamal <salonhamal278@gmail.com>
2023-11-03 22:47:04 +0530

    complete counter project

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [counter, setCounter] = useState(15)
   
-  let counter = 5;
+
 
   const addValue = () => {
-    counter = counter + 1;
+    
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter > 0 ) {
+      setCounter(counter - 1)
+    }
   }
 
   return (
     <>
-     <h1> Chai aur react </h1>
+     <h1> Counter aur react </h1>
      <h2>counter value: {counter}</h2>
 
      <button
@@ -19,7 +29,9 @@ function App() {
      > Add Value </button>
      <br />
      <br />
-     <button>Remove Value</button>
+     <button
+     onClick={removeValue}
+     >Remove Value</button>
     </>
   )
 }
